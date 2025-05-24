97813ec8e07d546fbf71b3f620fd9f101df660fc
thisIsHarshB <harshbalolia2006@gmail.com>
2025-05-24 16:22:08 +0530

    BACKEND COMPLETE.
    moving on to Frontend using React.

--- backend/src/sv.js
+++ backend/src/sv.js
@@ -8,7 +8,6 @@ dotenv.config();
 const PORT=process.env.PORT||5001;
 const app=express();
 
-connectDB();
 
 //middleware
 app.use(express.json()); // this middleware lets us access the elements by parsing them as JSON bodies. for eg, we can use req.body in our controllers because of this.
@@ -16,13 +15,15 @@ app.use(rateLimiter);
 
 // simple custom middleware
 // app.use((req, res, next)=>{
-//     console.log(`req method = ${req.method}, and req url = ${req.url}`);
-//     next();
-// });
-
+    //     console.log(`req method = ${req.method}, and req url = ${req.url}`);
+    //     next();
+    // });
+    
 app.use("/api/notes", notesRoutes);
-
-app.listen(PORT, () => {
-    console.log("Server started at PORT :", PORT);
+    
+connectDB().then(()=>{
+    app.listen(PORT, () => {
+        console.log("Server started at PORT :", PORT);
+    });
 });
 
